df1119a0f1ae49de424b2441ad522dc96f03359a
CQ <15939101882@163.com>
2020-11-28 22:51:14 +0800

    更改执行脚本14

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const child = process.spawnSync('ls',['-l','/usr'])
 let preveMd5 = null,
     fsWait = false
 // console.log('stdout here: \n' + child.stdout);
-// const filePath = '/Users/cq/Desktop/工作/GIT/shell/' 
-const filePath = '/www/server/' 
+// const filePath = '/Users/cq/Desktop/工作/GIT/Node/views/' 
+const filePath = '/www/server/'  
 const app = express()
 app.all('*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); //访问控制允许来源：所有
@@ -21,34 +21,37 @@ app.all('*', (req, res, next) => {
 })   
 console.log(`正在监听${filePath}`)
 watch.watchTree(filePath, function (f, curr, prev) {
+    
     if (typeof f == "object" && prev === null && curr === null) {   
          // Finished walking the tree 整个文件夹都遍历完了
-         console.log('1')
-         run()
+         console.log('11111')
+        //  run()
     } else if (prev === null) {  
           // f is a new file 新增文件了
-          console.log('新增文件了')
+          console.log('新增文件了',f)
     } else if (curr.nlink === 0) { 
-      console.log('删除文件了')    
+      console.log('删除文件了',f)    
        // f was removed 删除文件了 
-    } else {      
-      console.log(`${filePath}文件发生更新`) 
+    } else {  
+      console.log(f.indexOf('node_modules'))    
+      if(f.indexOf('node_modules') != -1) return false
+      console.log(`${f}文件发生更新`) 
       // f was changed 更改文件了
       run()
      
     }
 },
-function run() {
-  aa = process.execFile(`./nodeRun.sh`,['arg1','arg2','arg3'],{//分离和忽略的stdin是这里的关键：
-    detached:true,
-    stdio:[ 'ignore',1,2]
-  })
-  //和unref（）会以某种方式使孩子的事件循环与父母的事件循环分离：
-  aa.unref(); 
-  aa.stdout.on('data',function (data) {
-    console.log('shell***' + data);
-  });
-}
+// function run() {
+//   aa = process.execFile(`./nodeRun.sh`,['arg1','arg2','arg3'],{//分离和忽略的stdin是这里的关键：
+//     detached:true,
+//     stdio:[ 'ignore',1,2]
+//   })
+//   //和unref（）会以某种方式使孩子的事件循环与父母的事件循环分离：
+//   aa.unref(); 
+//   aa.stdout.on('data',function (data) {
+//     console.log('shell***' + data);
+//   });
+// }
   // (event,filename) => {
   // console.log('更改')
   // if (filename){
